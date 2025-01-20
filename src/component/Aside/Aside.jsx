ae31ad6d9cc2af619fb35ad73090fe6efe4c1603
Plescan Andrei Leonard <leonardplescan@gmail.com>
2025-01-20 22:03:31 +0100

    cartella backend + connectDB + routes

--- src/component/Aside/Aside.jsx
+++ src/component/Aside/Aside.jsx
@@ -1,50 +1,59 @@
-// Importo gli hooks di react 
-import React, { useState, useEffect } from "react";
+// Importo gli hooks di react
+import React, { useState, useEffect} from "react";
 
 // Importo gli stili di react-bootstrap
 import { Card, Container, Button, Row, Col } from "react-bootstrap";
 
 const Aside = () => {
-
   // useState utilizzato per prendere tutti gli utenti dall'endpoint /profile/ tramite la "get"
-  const [users, setUsers] = useState([]);
-  const [showAll, setShowAll] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState([]);
+  // const [showAll, setShowAll] = useState(false);
+  // const [loading, setLoading] = useState(true);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/profile/api/users`);
+      const data = await response.json();
+      setUser(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   // useEffect utilizzato per montare la "get" in pagina una volta sola
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch(
-          "https://striveschool-api.herokuapp.com/api/profile/",
-          {
-            headers: {
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzRmNTJjMTIyY2EwMzAwMTU0ODg0YjEiLCJpYXQiOjE3MzMyNTE3NzcsImV4cCI6MTczNDQ2MTM3N30.2ILN61xsYSCkFQcH7evxZxTq8xCfbieJfh2FBphi8NQ",
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        const data = await response.json();
-        setUsers(data);
-      } catch (error) {
-        console.error("Errore nel caricamento degli utenti", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchUsers(); // Chiamata alla funzione per recuperare gli utenti
+    fetchUsers();
+    // const fetchUsers = async () => {
+    //   try {
+    //     const response = await fetch(
+    //       "https://striveschool-api.herokuapp.com/api/profile/",
+    //       {
+    //         headers: {
+    //           Authorization:
+    //             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzRmNTJjMTIyY2EwMzAwMTU0ODg0YjEiLCJpYXQiOjE3MzMyNTE3NzcsImV4cCI6MTczNDQ2MTM3N30.2ILN61xsYSCkFQcH7evxZxTq8xCfbieJfh2FBphi8NQ",
+    //           "Content-Type": "application/json",
+    //         },
+    //       }
+    //     );
+    //     const data = await response.json();
+    //     setUsers(data);
+    //   } catch (error) {
+    //     console.error("Errore nel caricamento degli utenti", error);
+    //   } finally {
+    //     setLoading(false);
+    //   }
+    // };
+    // fetchUsers(); // Chiamata alla funzione per recuperare gli utenti
   }, []);
 
-  // Blocco if per il Loading degli utenti 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+  // Blocco if per il Loading degli utenti
+  // if (loading) {
+  //   return <p>Loading...</p>;
+  // }
 
-// Se il flag `showAll` è vero, assegna tutti gli utenti (array `users`) a `displayedUsers`.
-// Altrimenti, assegna solo i primi 5 utenti dall'array utilizzando il metodo `slice`.
-const displayedUsers = showAll ? users : users.slice(0, 5);
+  // Se il flag `showAll` è vero, assegna tutti gli utenti (array `users`) a `displayedUsers`.
+  // Altrimenti, assegna solo i primi 5 utenti dall'array utilizzando il metodo `slice`.
+  // const displayedUsers = showAll ? users : users.slice(0, 5);
 
   return (
     <aside className="mt-5 pt-2">
@@ -61,68 +70,64 @@ const displayedUsers = showAll ? users : users.slice(0, 5);
             overflowY: "auto", // Permette di scrollare solo la parte delle card
           }}
         >
-          {displayedUsers.map((user) => (
-            <Col key={user._id} sm={12} className="mb-1">
-              <Card
-                className="d-flex flex-row flex-wrap border-0"
-                style={{ width: "100%" }}
+          <Col key={user._id} sm={12} className="mb-1">
+            <Card
+              className="d-flex flex-row flex-wrap border-0"
+              style={{ width: "100%" }}
+            >
+              <Col
+                md={4}
+                className="d-flex justify-content-center align-items-center"
               >
-                <Col
-                  md={4}
-                  className="d-flex justify-content-center align-items-center"
-                >
-                  <Card.Img
-                    src={user.image}
-                    alt={`${user.name} ${user.surname}`}
-                    style={{
-                      width: "50px",
-                      height: "50px",
-                      objectFit: "cover",
-                    }}
-                    className="img-fluid rounded-circle"
-                  />
-                </Col>
-                <Col md={8}>
-                  <Card.Body className="p-0">
-                    <Card.Title style={{ fontSize: "13px" }}>
-                      {user.name} {user.surname}
-                    </Card.Title>
-                    <Card.Text className="mb-1" style={{ fontSize: "12px" }}>
-                      {user.title || ""}
-                    </Card.Text>
-                    <Button variant="primary" size="sm">
-                      Collegati
-                    </Button>
-                  </Card.Body>
-                </Col>
-              </Card>
-              <hr style={{ border: "1px solid #ddd", margin: "10px 0" }} />
-            </Col>
-          ))}
+                <Card.Img
+                  src={user.image}
+                  alt={`${user.name} ${user.surname}`}
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                    objectFit: "cover",
+                  }}
+                  className="img-fluid rounded-circle"
+                />
+              </Col>
+              <Col md={8}>
+                <Card.Body className="p-0">
+                  <Card.Title style={{ fontSize: "13px" }}>
+                    {user.name} {user.surname}
+                  </Card.Title>
+                  <Card.Text className="mb-1" style={{ fontSize: "12px" }}>
+                    {user.title || ""}
+                  </Card.Text>
+                  <Button variant="primary" size="sm">
+                    Collegati
+                  </Button>
+                </Card.Body>
+              </Col>
+            </Card>
+            <hr style={{ border: "1px solid #ddd", margin: "10px 0" }} />
+          </Col>
         </Row>
         {/* Bottone "Mostra tutto" */}
-        {!showAll && users.length > 5 && (
-          <div
-            className="d-flex justify-content-center"
+        <div
+          className="d-flex justify-content-center"
+          style={{
+            position: "absolute",
+            bottom: "10px",
+            width: "100%",
+            paddingBottom: "10px",
+          }}
+        >
+          <Button
+            variant="outline-primary"
+            size="lg"
+            // onClick={() => setShowAll(true)}
             style={{
-              position: "absolute",
-              bottom: "10px",
-              width: "100%",
-              paddingBottom: "10px",
+              fontSize: "15px",
             }}
           >
-            <Button
-              variant="outline-primary"
-              size="lg"
-              onClick={() => setShowAll(true)}
-              style={{
-                fontSize: "15px",
-              }}
-            >
-              Mostra tutto
-            </Button>
-          </div>
-        )}
+            Mostra tutto
+          </Button>
+        </div>
       </Container>
     </aside>
   );
